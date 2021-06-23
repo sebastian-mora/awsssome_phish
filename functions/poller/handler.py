e430d069af587428887cb69ea6286ef25ab4df3c
seb <seb@ruse.tech>
2021-06-23 14:26:37 -0700

    updated

--- functions/poller/handler.py
+++ functions/poller/handler.py
@@ -1,12 +1,27 @@
 import json
 
 
+# Create dyamodb table 
+# write loop
+
+# test token active (wont work with mock data)
+
+
+# if active then publish message to sns 
+
+# update db table sessionCaptured timestamp
+
+# triggered by clouwatch (this is in serverless documentiaon. Trigger every 1 min )
+
+
 def main(event, context):
+
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
     }
 
+    
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
--- functions/urlRedirect/handler.py
+++ functions/urlRedirect/handler.py
@@ -1,10 +1,48 @@
 import json
+from threading import main_thread
+import boto3
+import os
 
+def create_oidc_application(sso_oidc_client):
+    print("Creating temporary AWS SSO OIDC application")
+    client = sso_oidc_client.register_client(
+        clientName='aws-org-mapper',
+        clientType='public'
+    )
+    client_id = client.get('clientId')
+    client_secret = client.get('clientSecret')
+    return client_id, client_secret
+
+
+def initiate_device_code_flow(sso_oidc_client, client_id, client_secret, start_url):
+    print("Initiating device code flow")
+    authz = sso_oidc_client.start_device_authorization(
+        clientId=client_id,
+        clientSecret=client_secret,
+        startUrl=start_url
+    )
+
+    url = authz.get('verificationUriComplete')
+    deviceCode = authz.get('deviceCode')
+    return url, deviceCode
+
+
+def create_device_code_url(sso_oidc_client, start_url):
+    oidc_application = create_oidc_application(sso_oidc_client)
+    url, device_code = initiate_device_code_flow(
+        sso_oidc_client, oidc_application, start_url)
+    return url, device_code, oidc_application
 
 def main(event, context):
+
+    START_URL = os.environ['START_URL']
+    REGION = os.environ['REGION']
+    sso = boto3.client('sso', region_name=REGION)
+
+    url = create_device_code_url(sso, START_URL)
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "input": url
     }
 
     response = {
@@ -14,11 +52,4 @@ def main(event, context):
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+print(main(None, None))
